8f7aa2a0cc4934561b2c9969fdeb52e7b39e8556
Georgy Guminov <gogagum@gmail.com>
2021-05-24 19:59:31 +0000

    Restart after fail.

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 from src.bot import bot
+import logging
 
 if __name__ == "__main__":
-    bot.polling(timeout=200)
+    while True:
+        try:
+            bot.polling(timeout=200)
+        except Exception as err:
+            # In case of internet error try starting again
+            logging.debug(err)
+            time.sleep(5)
